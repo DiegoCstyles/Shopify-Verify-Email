418275d55c3cac4348432356785711a4dc109d6e
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2024-03-11 15:56:56 -0300

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,27 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
-import fetch from 'node-fetch';
+// Replace with your actual Shopify store credentials
+const shopifyStore = process.env.shopifyStore;
+const apiVersion = process.env.apiVersion;
+const accessToken = process.env.accessToken;
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
 app.post('/Shopify-Verify-Email', async (req, res) => {
   const emailField = req.body.email;
-  
+
   try {
-    const response = await fetch(`https://${shopifyStore}/admin/api/${apiVersion}/customers.json`, {
-      method: 'GET',
+    const response = await axios.get(`https://${shopifyStore}/admin/api/${apiVersion}/customers.json`, {
       headers: {
         'Content-Type': 'application/json',
         'X-Shopify-Storefront-Access-Token': storefrontAccessToken,
       },
     });
 
-    if (response.ok) {
-      const data = await response.json();
-      
+    if (response.status === 200) {
+      const data = response.data;
+
       // Assuming customFieldValues.Email contains the email to filter
       const filteredCustomer = data.customers.find(customer => customer.email === emailField);
 
@@ -34,7 +39,7 @@ app.post('/Shopify-Verify-Email', async (req, res) => {
       return res.status(response.status).send(response.statusText); // Handle error response
     }
   } catch (error) {
-    console.error('Error fetching customers:', error);
+    console.error('Error fetching customers:', error.message);
     return res.status(500).send('Internal Server Error'); // Return an appropriate error response
   }
 });
